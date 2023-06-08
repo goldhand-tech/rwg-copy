e68904c3a538983f8cbfd1b18920814430356990
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-06-08 15:28:08 +0200

    ReponsiveLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { GridItem, Show, Grid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav""main"`,
+          lg: `"nav nav" "aside main"`, //1024 px larger
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above={"lg"}>
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
