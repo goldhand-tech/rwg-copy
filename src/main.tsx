0c6bf43da5c5bcd5d6520578008eb12d321844ca
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-06-08 15:57:46 +0200

    built color mode switch

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
+import theme from "./theme";
+import { ColorModeScript, ColorModeScriptProps } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>
