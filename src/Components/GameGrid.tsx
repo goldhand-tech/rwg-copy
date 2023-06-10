0194fbce4906c5fee0d141afd4b9c5ac9f7d378c
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-06-10 12:12:41 +0200

    building game card

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -1,6 +1,7 @@
 import useGames from "../Hooks/useGames";
 import apiClient from "../Services/api-client";
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import { GameCard } from "./GameCard";
 
 interface Game {
   id: number;
@@ -18,11 +19,15 @@ export const GameGrid = () => {
   return (
     <>
       {error ?? <Text>{error}</Text>}
-      <ul>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard game={game}></GameCard>
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
