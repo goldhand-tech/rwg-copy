bc144676749f4228f521f12e36725f65b338e0c5
Pop Lorenzo <lorenzo.pop100@gmail.com>
2023-06-11 09:44:48 +0200

    Highlight the selected genre

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -12,9 +12,10 @@ import getCroppedImageUrl from "../Services/images-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-export const GenreList = ({ onSelectGenre }: Props) => {
+export const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) {
@@ -39,6 +40,7 @@ export const GenreList = ({ onSelectGenre }: Props) => {
                 fontSize={"lg"}
                 variant={"link"}
                 onClick={() => onSelectGenre(genre)}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               >
                 {" "}
                 {genre.name}
